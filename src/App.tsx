9fa976d9861dc3ff85c631bb028f0797415aa840
Con Sınov <hakanunal_1996@hotmail.com>
2024-10-16 23:36:55 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { Row, Typography, Col, Card } from "antd";
-
-const { Meta } = Card;
+import { Row, Typography, Col } from "antd";
 
 const { Title, Paragraph } = Typography;
 
@@ -11,8 +9,98 @@ const App = () => (
         Proje Yönetimi
       </Title>
       <Paragraph>
+        Yazılım içerisinde projenin anlaşma süreciyle birlikte başlayan
+        geliştirme ve sonrasındaki bakım süreçleri düşünüldüğünde müşteri ve
+        satıcı arasında ürünün varlığı devam edeceği süre boyunca bitmeyecek bir
+        iletişim başlamış olur. Burada mevcut ekibi ve ürünü yönetmek ise
+        günümüz koşullarında teknik bilgi eksikliği veya yetersizliğinden insan
+        davranışıyla anlaşılabilmekte. Aşağıda benzer örneklerle konuyu biraz
+        daha anlaşılır hale getirebilmek mümkün.
       </Paragraph>
-      
+
+      <Paragraph>
+        - 10 kişilik bir ekip ve onlarla birlikte 1 yöneticiyi ürüne
+        atadığınızda geliştirme aşamalarında ilerledikçe ve çıktı almaya
+        başladıkça ekiple satranç tahtası üzerindeki gibi oynama yaparak kimin
+        nelere katkı verdiğini veya vermediğini görebilirsiniz veya daha da
+        ilginci kim neyin bitip bitmediğinin farkında gibi bilgilere
+        ulaşabilirsiniz. Burada büyük resimde projenin veya ürünün istenen
+        şeklini bilen sadece az sayıda insan olduğundan süreci 3. göz olarak
+        izlemek daha kolaydır. Burada zor olan yönetim tarafı için ürünün sadece
+        dışarıdan görülen kısmı için istenen noktaya gelip gelmediğidir. Teknik
+        olarak eksikler riskler veya varsa hatalar nelerdir anlayabilmek zordur.
+        Genelde bu noktalarda satranç tahtasındaki hamleler başlar. Bazı kişiler
+        farklı projelere kaydırılabilir bu noktada hem görev değişikliği olan
+        personelin genel tavrı anlaşılmaya çalışılır bununla birlikte ekipte
+        kalanların gidenle ilgili genel yorumunu edinmek mümkündür bu bir an
+        değil genelde uzun bir süreçtir. Burada ekip küçülebilir büyüyebilir
+        hatta proje değişikliği yerine doğrudan işten çıkarma bile yapılabilir
+        çünkü işi tamamladığını bilen birisinin davranışıyla eksiklerinin
+        olduğunu bilen birinin davranışıyla veya hatta daha farklı durumlarda
+        yine insan davranışı ile anlaşılabilmeye çalışılır. Burada hayatın
+        içerisinden örnek vermek gerekirse endişeli olduğunuz bir gün
+        muhattabınız size isminizle hitap ederse panikleyebilirsiniz endişeniz
+        olmadığı günde çok normal bir şekilde sakin şekilde yanıt verirsiniz.
+        Özetlemek gerekirse yöneticileriniz genelde sizden daha fazla bilgiye
+        sahip değillerdir ancak oluşturduğu pozisyon ve doldurduğu otorite
+        boşluğuyla bazen hiç farkında olmadan bile olumlu veya olumsuz itiraflar
+        verirsiniz.
+      </Paragraph>
+
+      <Paragraph>
+        * Vakti zamanında hükümdarlığın birinde bir halk varmış padişah refahı
+        arttırmak için hazinenin başına ışıltılı bir veznedarı getirir. Görevi
+        boyunca ışıltısıyla gözleri kamaştıran görkemli kaftanıyla hava atarmış.
+        Gel zaman git zaman işler yolunda gitmemeye başlar. Halk fakirleşir ama
+        durum anlaşılmaz veznedar hem sarayda hem halkın arasında sürekli nabız
+        yoklar. İleride düzelecek hayal edin her şey güzel olacak diyip bir
+        yandan hayal satıp bir yandan kim farkında kim ne kadar farkında değil
+        puslu havayı sezmeye çalışırmış çünkü kendisinin ve çevresinin çıkarını
+        halkın parasında görürmüş. Halktan sesler yükselmeye başladıkça
+        sarayında oturan padişah duruma anlam vermeye çalışırmış çünkü veznedar
+        çok sıradışı hiç bir yerde olmayan şeyler yapmaktaymış ama veznedar ve
+        çevreside aynı şekilde tam tersi yorum yaptıkça durum daha da
+        karışmaktaymış. Bir zaman geçip işler hala yoluna girmeyince dayanamayan
+        padişah halkın içinden birisini işleri yoluna koyması için hazinesinin
+        başına atamış. Zaman geçtikçe halktan sesler azalmaya eskiden konuşulan
+        konular konuşulmaya başlamış padişah durumun yavaş yavaş normale
+        dödüğünü bu şekilde düşünmüş ama yetinmemiş. Halkın içinden gelip
+        aslanlar gibi işini yapan veznedarı da görevden almış sonra hem halkın
+        hem veznedarın tepkisini sarayda izlemişler. Veznedar yaptıklarından
+        eminmiş sonrasında gelen veznedar da onun yaptığı işlerin üzerinden
+        devam edeceğini olağan bir şekilde söylemiş halktan gelen tepkiler ve
+        davranış yine durumun eskisi kadar olmasa da normale döndüğünü
+        gösterirmiş. Bunun üzerine hükümdarlıkta yaşanan bu hikayeyi duyan uzak
+        diyarlarda bir komutan padişaha mektup yazmış. Mektup aşağıdadır.
+      </Paragraph>
+
+      <Paragraph>
+        "Halkın durduk yere fakirleşti göz göre göre olan oldu sen hainin
+        görevden alınmasıyla mı yetineceksin bu kime revadır ? Eğer sen
+        padişahsan emir ver kellesini alsınlar eğer ben gelirsem zaten kellesini
+        alırım demiş."
+      </Paragraph>
+
+      <Paragraph>
+        Özetlemek gerekirse çoğu zaman karşımızdakileride kendimizin bildiği
+        şeyleri bildiğini veya bilmediğini düşünerek davranırız.
+        Söylediklerimizde yaptıklarımızda davranışlarımızda bu noktada bazen
+        durup düşünmeli geçmişteki ve gelecekteki olabilecek olaylara
+        bakabilmeliyiz. Söylediklerimizle düşündüklerimizin paralel olduğu
+        anlarda bunları paylaşabilmeli ve toplum içerisinde aktarım
+        yapabilmeliyiz. Nereden aykırı ses geldiği aykırı sesin gerçekten aykırı
+        olduğu için mi geldiği yoksa farklı bir nedenden dolayı mı geldiği gibi
+        durumlar uzun soluklu anlaşılabilmekle birlikte bizi destekleyenlerin de
+        gerçekten düşündükleri ve söyledikleri birbiriyle paralel şekilde bizi
+        destekliyorlar mı yoksa mış gibi mi yapıyorlar yapıyorlarsa neden
+        yapıyorlar farkedebilmek ve alınan sorumluluklar ölçüsünde kendimizi
+        toplum içerisinde hem güvende hem de yalnız olmayacak şekilde
+        konumlayabilmemiz önemli bir konu. İçgüdü, davranış ve psikoloji
+        yalnız olduğunuz bir toplum içerisinde düşe kalka hata yaparak bu
+        hataları gelecekte tecrübe olarak kullanarak öğrendiğimiz uzun soluklu
+        yolculuk ve bu yolculukta sadece kendimizin değil çevremizdeki
+        insanlarında neler yaptıllarından öğrenerek ilerliyoruz. 
+      </Paragraph>
     </Col>
   </Row>
 );
